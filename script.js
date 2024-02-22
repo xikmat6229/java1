237e630c6e5b39905f7fb503ed739eb632ba6115
xikmatilla6229 <xikmatilla996@gmail.com>
2024-02-22 20:14:45 +0500

    java1

---
+++ script.js
@@ -0,0 +1,8 @@
+let a=prompt('Ismingizni kiriting')
+let b=prompt('Familiyangiznizni kiriting')
+let d=prompt('Yoshingizni kiriting')
+let c=prompt('Yoqtirgan faninggizni kiriting')
+let e=prompt('Yashaydigan Tumaningizni kiriting')
+
+alert('malumotingizni kiritganingiz uchun raxmat')
+console.log(a,b,d,c);
